b60f5c396079309b4e234520b41fa681858ffbe8
harshureddy06 <Harshithapunyapu3092@gmail.com>
2023-01-14 13:14:15 +0530

    todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,4 +1,20 @@
+const request = require("supertest");
 
+const db = require("../models/index");
+const app = require("../app");
+
+let server, agent;
+
+describe("Todo test suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3000, () => {});
+    agent = request.agent(server);
+  });
+  afterAll(async () => {
+    await db.sequelize.close();
+    server.close();
+  });
   test("Creates a todo and responds with json at /todos POST endpoint", async () => {
     const response = await agent.post("/todos").send({
       title: "Buy milk",
